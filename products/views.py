ab338095f62d7d00e988d569ac76059f67c7cd4d
tdawes93 <tdawes136@googlemail.com>
2022-08-30 23:50:34 +0000

    Create tour list view and url and hook up to main urls

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Tour
 
-# Create your views here.
+
+def all_tours(request):
+    """
+    View to display the list of tours available
+    """
+    tours = Tour.objects.all().order_by('friendly_name')
+
+    context = {
+        'tours': tours,
+    }
+
+    return render(request, 'products/tours.html', context)
