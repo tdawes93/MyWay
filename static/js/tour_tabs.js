29aedd7067045045244f3d613a93aa93ee2ff6a3
tdawes93 <tdawes136@googlemail.com>
2022-09-02 17:24:02 +0000

    Add tab dropdown structure for dates

--- static/js/tour_tabs.js
+++ static/js/tour_tabs.js
@@ -11,3 +11,31 @@ function openTab(evt, tabName) {
   document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
+function openYear(evt, yearName) {
+  var i, yearcontent, yearlinks;
+  yearcontent = document.getElementsByClassName("yearcontent");
+  for (i = 0; i < yearcontent.length; i++) {
+    yearcontent[i].style.display = "none";
+  }
+  yearlinks = document.getElementsByClassName("yearlinks");
+  for (i = 0; i < yearlinks.length; i++) {
+    yearlinks[i].className = yearlinks[i].className.replace(" active", "");
+  }
+  document.getElementById(yearName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+function openMonth(evt, monthName) {
+  var i, monthcontent, monthlinks;
+  monthcontent = document.getElementsByClassName("monthcontent");
+  for (i = 0; i < monthcontent.length; i++) {
+    monthcontent[i].style.display = "none";
+  }
+  monthlinks = document.getElementsByClassName("monthlinks");
+  for (i = 0; i < monthlinks.length; i++) {
+    monthlinks[i].className = monthlinks[i].className.replace(" active", "");
+  }
+  document.getElementById(monthName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
