1129fd05f17a4fe59e34b86b3efb5538f9536ce0
tdawes93 <tdawes136@googlemail.com>
2022-09-06 19:28:52 +0000

    Add add_to_bag view, url and functionality

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_bag(request):
@@ -6,3 +6,22 @@ def view_bag(request):
     A view to return the bag and contents
     """
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, tour_id):
+    """
+    A view to add the date of the specified product to the bag
+    """
+    date = request.POST.get('calendar')
+    redirect_url = request.POST.get('redirect_url')
+
+    bag = request.session.get('bag', {})
+
+    if tour_id in list(bag.keys()):
+        bag[tour_id] += date
+    else:
+        bag[tour_id] = date
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
