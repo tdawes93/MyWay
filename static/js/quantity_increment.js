d92d5eef9fc15714befe6b5d629d4dae54483934
tdawes93 <tdawes136@googlemail.com>
2022-09-09 03:25:08 +0000

    Add update and remove links to update bag item quantities and JQuery to handle these

--- static/js/quantity_increment.js
+++ static/js/quantity_increment.js
@@ -34,3 +34,22 @@ $('.decrease-qty').click(function (e) {
     var currentValue = parseInt($(closestInput).val());
     $(closestInput).val(currentValue - 1);
 });
+
+    // Update quantity on click
+    $('.update-link').click(function(e) {
+        var form = $(this).prev('.update-form');
+        form.submit();
+    })
+
+    // Remove item and reload on click
+    $('.remove-item').click(function(e) {
+        var csrfToken = "{{ csrf_token }}";
+        var itemId = $(this).attr('id').split('remove_')[1];
+        var url = `/bag/remove/${itemId}`;
+        var data = {'csrfmiddlewaretoken': csrfToken};
+
+        $.post(url, data)
+         .done(function() {
+             location.reload();
+         });
+    })
