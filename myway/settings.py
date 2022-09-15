2a826ea012858fd459e44525532115136f6f8a2e
tdawes93 <tdawes136@googlemail.com>
2022-09-15 23:48:09 +0000

    Add class method to webhook handler and webhook view

--- myway/settings.py
+++ myway/settings.py
@@ -214,3 +214,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STRIPE_CURRENCY = 'cad'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
