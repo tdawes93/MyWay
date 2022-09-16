5afbd78b55f4f1d0b41487d44d5c7465d150f411
tdawes93 <tdawes136@googlemail.com>
2022-09-16 04:54:17 +0000

    Add view, url and create template structue for profile page

--- myway/urls.py
+++ myway/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('tours/', include('products.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
     path('summernote/', include('django_summernote.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
