89cbe2da851984f3cc9157e5439dd3715561a94a
tdawes93 <tdawes136@googlemail.com>
2022-09-17 12:12:35 +0000

    Add D.o.B to profile model

--- profiles/models.py
+++ profiles/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.core.validators import MaxValueValidator
+from datetime import date
 
 from django_countries.fields import CountryField
 
@@ -12,12 +14,16 @@ class Profile(models.Model):
     delivery information and order history
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_of_birth = models.DateField(
+        null=True, blank=True,
+        validators=[MaxValueValidator(limit_value=date.today)]
+    )
     default_phone_number = models.CharField(
-        max_length=20, null=True, blank=True)    
+        max_length=20, null=True, blank=True)
     default_street_address1 = models.CharField(
         max_length=80, null=True, blank=True)
     default_street_address2 = models.CharField(
-        max_length=80, null=True, blank=True)    
+        max_length=80, null=True, blank=True)
     default_town_or_city = models.CharField(
         max_length=40, null=True, blank=True)
     default_county = models.CharField(
