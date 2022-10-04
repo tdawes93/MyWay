65cd8757970a3821145ae9aab69a112179352499
tdawes93 <tdawes136@googlemail.com>
2022-10-04 13:35:50 +0000

    Add development variable to settings

--- myway/settings.py
+++ myway/settings.py
@@ -118,6 +118,8 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
+DEVELOPMENT = 1
+
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
