839d03c69518a40616a976a89a687445a0e9f974
tdawes93 <tdawes136@googlemail.com>
2022-10-04 15:36:53 +0000

    Edit allowed hosts

--- myway/settings.py
+++ myway/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['tdawes93-myway-tours.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['myway-tours.herokuapp.com', 'localhost']
 
 
 # Application definition
