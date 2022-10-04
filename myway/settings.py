90c99a987c21dd8b228dd7c43f02e46ca37a1aa6
tdawes93 <tdawes136@googlemail.com>
2022-10-04 17:58:24 +0000

    Fix spelling error in aws settings

--- myway/settings.py
+++ myway/settings.py
@@ -217,11 +217,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-    AWS_SOTRAGE_BUCKET_NAME = 'myway-tours'
+    AWS_STORAGE_BUCKET_NAME = 'myway-tours'
     AWS_S3_REGION_NAME = 'us-east-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_SOTRAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
