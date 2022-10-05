4abd7f12d1d42247c6fd950681810e2c2893260b
tdawes93 <tdawes136@googlemail.com>
2022-10-05 19:42:58 +0000

    Set up sending real emails

--- myway/settings.py
+++ myway/settings.py
@@ -120,10 +120,6 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-DEVELOPMENT = 1
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
@@ -254,5 +250,16 @@ STRIPE_CURRENCY = 'cad'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
-DEFAULT_FROM_EMAIL = 'myway.tours@example.com'
-DEFAULT_CONTACT_NUMBER = '+1 (234) 567-8910'
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'myway.tours@example.com'
+    DEFAULT_CONTACT_NUMBER = '+1 (234) 567-8910'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smpt.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smpt.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
--- static/js/tour_tabs.js
+++ static/js/tour_tabs.js
@@ -1,68 +1,68 @@
 jQuery(document).ready(function ($) {
-  $(document).ready(function () {
-    $(window).on('load', (function (e) {
+    $(document).ready(function () {
+      $(window).on('load', (function (e) {
+        openTab(e, 'the-trip')
+        document.getElementById('defaultOpen').className += " active";
+      }))
+    })
+
+    $('.view-date').click(function (e) {
+      openTab(e, 'dates')
+    });
+
+    $('#defaultOpen').click(function (e) {
       openTab(e, 'the-trip')
-      document.getElementById('defaultOpen').className += " active";
-    }))
-  })
+    });
 
-  $('.view-date').click(function (e) {
-    openTab(e, 'dates')
-  });
+    $('.yearlinks').click(function (e) {
+      var year = $(this).data('year_id');
+      openYear(e, year);
+    })
 
-  $('#defaultOpen').click(function (e) {
-    openTab(e, 'the-trip')
-  });
+    $('.monthlinks').click(function (e) {
+      var month = $(this).data('month_id');
+      openMonth(e, month);
+    })
 
-  $('.yearlinks').click(function (e) {
-    var year = $(this).data('year_id');
-    openYear(e, year);
-  })
-
-  $('.monthlinks').click(function (e) {
-    var month = $(this).data('month_id');
-    openMonth(e, month);
-  })
-
-  function openTab(evt, tabName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+    function openTab(evt, tabName) {
+      var i, tabcontent, tablinks;
+      tabcontent = document.getElementsByClassName("tabcontent");
+      for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+      }
+      tablinks = document.getElementsByClassName("tablinks");
+      for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+      }
+      document.getElementById(tabName).style.display = "block";
+      evt.currentTarget.className += " active";
     }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
-  }
 
-  function openYear(evt, yearName) {
-    var i, yearcontent, yearlinks;
-    yearcontent = document.getElementsByClassName("yearcontent");
-    for (i = 0; i < yearcontent.length; i++) {
-      yearcontent[i].style.display = "none";
+    function openYear(evt, yearName) {
+      var i, yearcontent, yearlinks;
+      yearcontent = document.getElementsByClassName("yearcontent");
+      for (i = 0; i < yearcontent.length; i++) {
+        yearcontent[i].style.display = "none";
+      }
+      yearlinks = document.getElementsByClassName("yearlinks");
+      for (i = 0; i < yearlinks.length; i++) {
+        yearlinks[i].className = yearlinks[i].className.replace(" active", "");
+      }
+      document.getElementById(yearName).style.display = "block";
+      evt.currentTarget.className += " active";
     }
-    yearlinks = document.getElementsByClassName("yearlinks");
-    for (i = 0; i < yearlinks.length; i++) {
-      yearlinks[i].className = yearlinks[i].className.replace(" active", "");
-    }
-    document.getElementById(yearName).style.display = "block";
-    evt.currentTarget.className += " active";
-  }
 
-  function openMonth(evt, monthName) {
-    var i, monthcontent, monthlinks;
-    monthcontent = document.getElementsByClassName("monthcontent");
-    for (i = 0; i < monthcontent.length; i++) {
-      monthcontent[i].style.display = "none";
+    function openMonth(evt, monthName) {
+      var i, monthcontent, monthlinks;
+      monthcontent = document.getElementsByClassName("monthcontent");
+      for (i = 0; i < monthcontent.length; i++) {
+        monthcontent[i].style.display = "none";
+      }
+      monthlinks = document.getElementsByClassName("monthlinks");
+      for (i = 0; i < monthlinks.length; i++) {
+        monthlinks[i].className = monthlinks[i].className.replace(" active", "");
+      }
+      document.getElementById(monthName).style.display = "block";
+      evt.currentTarget.className += " active";
     }
-    monthlinks = document.getElementsByClassName("monthlinks");
-    for (i = 0; i < monthlinks.length; i++) {
-      monthlinks[i].className = monthlinks[i].className.replace(" active", "");
-    }
-    document.getElementById(monthName).style.display = "block";
-    evt.currentTarget.className += " active";
-  }
 });
