7a1d6552cefded4163d8de7c879c38926e10c011
tdawes93 <tdawes136@googlemail.com>
2022-10-06 23:58:22 +0000

    uncomment out custom 404 for tests

--- myway/urls.py
+++ myway/urls.py
@@ -16,4 +16,4 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# handler404 = 'myway.views.error404'
+handler404 = 'myway.views.error404'
