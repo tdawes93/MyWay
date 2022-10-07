cad981888d7b0128adbe0aacfea12a234d1d0ee8
tdawes93 <tdawes136@googlemail.com>
2022-10-07 13:13:49 +0000

    Add basic structure to footer pages

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='homepage'),
+    path('about_us/', views.about_us, name='about_us'),
+    path('destinations/', views.destinations, name='destinations'),
+    path('code_of_conduct/', views.code_of_conduct, name='code_of_conduct'),
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,27 @@ def home(request):
     A view to return the homepage
     """
     return render(request, 'home/index.html')
+
+
+def about_us(request):
+    """
+    A view to load the about us page
+    """
+    template = 'home/footer/about-us.html'
+    return render(request, template)
+
+
+def destinations(request):
+    """
+    A view to load the destinations page
+    """
+    template = 'home/footer/destinations.html'
+    return render(request, template)
+
+
+def code_of_conduct(request):
+    """
+    A view to load the code of conduct page
+    """
+    template = 'home/footer/code-of-conduct.html'
+    return render(request, template)
