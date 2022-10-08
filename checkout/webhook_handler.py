dd629f10abdf5e0109527b239968a4cf4b4dad75
tdawes93 <tdawes136@googlemail.com>
2022-10-08 03:08:18 +0000

    Remove print statement for debugging

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -56,7 +56,6 @@ class StripeWH_Handler:
 
         billing_details = intent.charges.data[0].billing_details
         shipping_details = intent.shipping
-        print(billing_details)
         print(shipping_details)
 
         grand_total = round(intent.charges.data[0].amount / 100, 2)
