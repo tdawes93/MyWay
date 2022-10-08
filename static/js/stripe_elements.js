59a3a13cadae8bfcbaef4b404b378c4e34e81df4
tdawes93 <tdawes136@googlemail.com>
2022-10-08 02:57:52 +0000

    Add shipping details to try and fix duplicate order bug

--- static/js/stripe_elements.js
+++ static/js/stripe_elements.js
@@ -70,7 +70,20 @@ jQuery(document).ready(function ($) {
                         }
                     }
                 },
-                
+                shipping: {
+                    name: $.trim(form.name.value),
+                    email: $.trim(form.email.value),
+                    phone: $.trim(form.phone_number.value),
+                    address: {
+                        line1: $.trim(form.street_address1.value),
+                        line2: $.trim(form.street_address2.value),
+                        city: $.trim(form.town_or_city.value),
+                        postal_code: $.trim(form.postcode.value),
+                        country: $.trim(form.country.value),
+                        state: $.trim(form.county.value),
+                    },
+                }
+
             }).then(function (result) {
                 if (result.error) {
                     var errorDiv = document.getElementById('card-errors');
